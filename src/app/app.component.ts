532067d517f624653edf8b8565f236e333470e4b
johnebri <john.ebr@yahoo.com>
2019-10-11 19:16:07 +0200

    Main App

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<events-list></events-list>'
+})
+export class AppComponent {
+  title = 'myapp';
+}
